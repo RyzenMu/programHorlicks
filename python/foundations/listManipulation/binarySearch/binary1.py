f586bf937262f33559e1a93e99109eee5509245c
RyzenMu <muruga13in2022@gmail.com>
2023-08-16 04:57:07 +0530

    binary search

---
+++ python/foundations/listManipulation/binarySearch/binary1.py
@@ -0,0 +1,31 @@
+import random
+
+# Creating a random number and sorting it
+
+#create a new array
+arr = []
+
+#for loop to generate 10 random numbers and store it in the array
+for i in range(10):
+	ran = random.randint(2, 100)
+	arr.append(ran)
+
+
+print(arr)
+
+def sortingarr(arr):
+	for e in range (len(arr)-1):
+		for w in range(len(arr)-1):
+			if arr[w] > arr[w+1]:
+				temp = arr[w]
+				arr[w] = arr[w+1]
+				arr[w+1] = temp
+		
+		
+sortingarr(arr)
+
+
+#print array
+print(arr)
+
+
---
+++ python/foundations/listManipulation/binarySearch/binary2.py
@@ -0,0 +1,28 @@
+import math
+
+arr = [7, 97, 26, 4, 76, 6, 83, 69, 20, 95, 53, 43, 13, 76, 23, 97, 34, 90, 95, 39, 4, 56]
+
+searchingNumber = 69
+
+arr.sort(reverse = True)
+
+def binary(arr):
+	low = 0
+	mid = len(arr)/2
+	high = len(arr)
+	while(low < 22 ):
+		print("----------")
+		print("Rightlow is : ", math.ceil(low))
+		print("Rightmid is : ", math.ceil(mid))
+		print("Righthigh is : ", high)
+		print("Leftlow is : ", 0))
+		print("Leftmid is : ", ))
+		print("Lefthigh is : ", high)
+		print("----------")
+		low = mid
+		mid = math.ceil((high+low)/2)
+		
+	
+
+binary(arr)
+	
