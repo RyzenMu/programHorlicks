7577452007630dd1d9aa5869cfa290c063f9e3cc
RyzenMu <muruga13in2022@gmail.com>
2023-08-19 05:54:23 +0530

    binary5

---
+++ python/foundations/listManipulation/binarySearch/binary5.py
@@ -0,0 +1,24 @@
+def BinarySearch(list1, key):
+	low = 0
+	high = len(list1)-1
+	Found = False
+	while low <=high and not Found:
+		mid = (low+high)//2
+		if key == list1[mid]:
+			Found = True
+		elif key>list1[mid]:
+			low = mid+1
+		else:
+			high = mid-1
+	if Found == True:
+		print("Key is Found")
+	else:
+		print("Key is not Found")
+
+
+list1 = [23, 1, 4, 2, 3]
+list1.sort()
+print(list1)
+
+BinarySearch(list1, 23)
+		
