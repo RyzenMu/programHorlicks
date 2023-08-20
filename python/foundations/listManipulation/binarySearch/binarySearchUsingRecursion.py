e18283236884bc56346d8bb280e6d558d1aecbb0
RyzenMu <muruga13in2022@gmail.com>
2023-08-21 04:49:07 +0530

    binary search using recursion

---
+++ python/foundations/listManipulation/binarySearch/binarySearchUsingRecursion.py
@@ -0,0 +1,21 @@
+list = [2, 4, 6, 8, 10, 12]
+
+def binarySearch(list, num):
+	low = 0
+	high = len(list)-1
+	mid = ((low+high)//2)+1
+	if list[mid] == num:
+		return True
+	elif list[mid] < num:
+		low = mid+1
+	else:
+		high = mid-1	
+	binarySearch(list, num)
+	return False
+
+result = binarySearch(list, 5)
+
+if result == True:
+	print("Number Found")
+else:
+	print("Number Not Found")
