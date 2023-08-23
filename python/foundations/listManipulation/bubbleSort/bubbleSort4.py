c5772a972613f39730ed59e7a205c43362b37490
RyzenMu <muruga13in2022@gmail.com>
2023-08-24 05:27:09 +0530

    bubble sort

--- python/foundations/listManipulation/bubbleSort/bubbleSort4.py
+++ python/foundations/listManipulation/bubbleSort/bubbleSort4.py
@@ -1,13 +1,20 @@
-list = [5, 2, 7, 4, 3, 8, 2]
+list = [1, 2, 3, 4]
+list1 = [5, 86, 187, 86, 19, 489]
 
-def sort(list, num):
-	swapCount = 0
-	for r in range(len(list)-1):
-		for t in range((len(list)-1)-r):
-			if list[t] > list[t+1]:
-				temp = list[t]
-				list[t] = list[t+1]
-				list[t+1] = temp
+def sort(list):
+	for a in range(len(list)-1):#loop to check if the list is in ascending order
+		while not list[a] <= list[a+1]:
+			return list
+		else:
+			for r in range(len(list)-1):
+				for t in range((len(list)-1)-r):
+					if list[t] > list[t+1]:
+						temp = list[t]
+						list[t] = list[t+1]
+						list[t+1] = temp
 
+sort(list)
+sort(list1)
 
-sort(list, 8)
+print(list)
+print(list1)
