4e01a5b2af3d11667e27d01d89a2cfa91ddc18fc
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2025-02-14 07:00:23 +0530

    Update Whilea.java

--- java/foundations/whilea/Whilea.java
+++ java/foundations/whilea/Whilea.java
@@ -10,6 +10,8 @@ public class Whilea {
             System.out.println(s);
             s++;
             System.out.println(s);
+            ++s
+            System.out.println(s);    
         }
 
 
